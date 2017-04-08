6970e61d6e519c58e3f7b343fd6d83cc132b21da
LJG123 <2414663309@qq.com>
2017-04-08 09:27:49 +0800

    work1

---
+++ demo1/js/style.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ae on 2017/4/8.
+ */
+new Swiper(".swiper-container",{
+    pagination:".swiper-pagination",
+    paginationClickable:true,
+    autoplay:1000
+});
+
+new IScroll("#wrap",{
+    scrollX:true,
+    scrollY:false
+});
+
